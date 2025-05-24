c6794403ae264d6c1271f2ce2286f4d57968e836
mkwhang <mkhwang@cluedit.com>
2025-05-24 16:41:56 +0900

    add kafka, connector, cdc

---
+++ gifticon/config/connectors/register.sh
@@ -0,0 +1,5 @@
+# Gifticon 커넥터 등록
+curl -X POST -H "Content-Type: application/json" --data @postgres-gifticon-connector.json http://localhost:8083/connectors
+
+# Tag 커넥터 등록
+curl -X POST -H "Content-Type: application/json" --data @postgres-tag-connector.json http://localhost:8083/connectors
---
+++ gifticon/config/connectors/unregister.sh
@@ -0,0 +1,5 @@
+# Gifticon 커넥터 제거
+curl -X DELETE http://localhost:8083/connectors/product-connector
+
+# Tag 커넥터 제거
+curl -X DELETE http://localhost:8083/connectors/tag-connector
